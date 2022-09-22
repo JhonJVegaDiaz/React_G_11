6ec4a49b4d441a652e6cdd36f3d7b07d26f8bcbe
JhonJVegaDiaz <jhonjairovegadiaz@gmail.com>
2022-09-22 16:56:00 -0500

    inclusion de Login y signup

--- InstaYa/src/components/login/login.jsx
+++ InstaYa/src/components/login/login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   MDBBtn,
   MDBContainer,
@@ -10,7 +10,53 @@ import {
 }
 from 'mdb-react-ui-kit';
 
-function LoginComponent() {
+
+const MIN = 6;
+const MAX = 50;
+const GROUP = `[a-zñáéíóúüA-ZÁÉÍÓÚÜÑ]{${MIN},${MAX}}`;
+const VALIDATION = new RegExp(`^${GROUP}( ${GROUP})*$`);
+
+const CUSTOM_VALIDATION = (input) => {
+  const matchesRegex = VALIDATION.test(input.trim());
+
+  if (input.length < MIN) return `Se necesitan mínimo ${MIN} caracteres`;
+  else if (input.length > MAX * 4)
+    return `Se necesitan aceptan  máximo ${MAX * 4} caracteres`;
+  else if (!matchesRegex) return 'Formato inválido';
+  else return '';
+};
+
+
+const PASSWORD_VALIDATION = (input) => {
+  if (password.value.length <= 8) return "Debes ingresar una password con mas de 8 caracteres";
+  else return '';
+};
+
+const LoginComponent = () => {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorUser, setErrorUser] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
+
+  const changeHandlerUser = (event) => {
+    setUser(event.target.value);
+    const errorUser = CUSTOM_VALIDATION(event.target.value);
+    setErrorUser(errorUser);
+  };
+  const changeHandlerPassword = (event) => {
+    setPassword(event.target.value);
+    const errorPassword = PASSWORD_VALIDATION(event.target.value);
+    setErrorPassword(errorPassword);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if ((user!=='')
+    && (password!=='')
+    && (!Boolean(errorUser)) 
+    && (!Boolean(errorPassword))) {location.replace("/")};
+  };
+
   return (
     <MDBContainer fluid>
 
@@ -23,18 +69,20 @@ function LoginComponent() {
               <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
               <p className="text-dark-50 mb-5">Please enter your login and password!</p>
 
-              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-dark' label='Username' id='formControlLg' type='txt' size="lg"/>
-              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-dark' label='Password' id='formControlLg' type='password' size="lg"/>
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-dark' label='User Name' id='user' type='txt' size="lg" value={user} onChange={changeHandlerUser}/>
+              {Boolean(errorUser) && <p>{errorUser}</p>}
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-dark' label='Password' id='password' type='password' size="lg" value={password} onChange={changeHandlerPassword}/>
+              {Boolean(errorPassword) && <p>{errorPassword}</p>}
 
               <p className="small mb-3 pb-lg-2"><a class="text-dark-50" href="#!">Forgot password?</a></p>
-              <MDBBtn outline className='mx-2 px-5' color='success' size='lg'>
-                Login
-              </MDBBtn>
+
+              <button type="button" class="btn btn-outline-success mx-2 px-5" onClick={submitHandler}> Login </button>
 
               <div>
                 <p className="mb-0">Don't have an account? <a href="/SignUp" class="text-dark-50 fw-bold">Sign Up</a></p>
 
               </div>
+
             </MDBCardBody>
           </MDBCard>
 
--- InstaYa/src/components/signUp/signup.jsx
+++ InstaYa/src/components/signUp/signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   MDBBtn,
   MDBContainer,
@@ -10,7 +10,75 @@ import {
 }
 from 'mdb-react-ui-kit';
 
-function SignUpComponent() {
+
+const MIN = 6;
+const MAX = 50;
+const GROUP = `[a-zñáéíóúüA-ZÁÉÍÓÚÜÑ]{${MIN},${MAX}}`;
+const VALIDATION = new RegExp(`^${GROUP}( ${GROUP})*$`);
+
+const CUSTOM_VALIDATION = (input) => {
+  const matchesRegex = VALIDATION.test(input.trim());
+
+  if (input.length < MIN) return `Se necesitan mínimo ${MIN} caracteres`;
+  else if (input.length > MAX * 4)
+    return `Se necesitan aceptan  máximo ${MAX * 4} caracteres`;
+  else if (!matchesRegex) return 'Formato inválido';
+  else return '';
+};
+
+const EMAIL_VALIDATION = (input) => {
+  var formato_email = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+  if (!input.match(formato_email)) return "Debes ingresar un email electronico valido!";
+  else return '';
+};
+const PASSWORD_VALIDATION = (input) => {
+  if (password.value.length <= 8) return "Debes ingresar una password con mas de 8 caracteres";
+  else return '';
+};
+
+const SignUpComponent = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorName, setErrorName] = useState('');
+  const [errorEmail, setErrorEmail] = useState('');
+  const [errorUser, setErrorUser] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
+
+  const changeHandlerName = (event) => {
+    setName(event.target.value);
+    const errorName = CUSTOM_VALIDATION(event.target.value);
+    setErrorName(errorName);
+  };
+  const changeHandlerEmail = (event) => {
+    setEmail(event.target.value);
+    const errorEmail = EMAIL_VALIDATION(event.target.value);
+    setErrorEmail(errorEmail);
+  };
+  const changeHandlerUser = (event) => {
+    setUser(event.target.value);
+    const errorUser = CUSTOM_VALIDATION(event.target.value);
+    setErrorUser(errorUser);
+  };
+  const changeHandlerPassword = (event) => {
+    setPassword(event.target.value);
+    const errorPassword = PASSWORD_VALIDATION(event.target.value);
+    setErrorPassword(errorPassword);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if ((name!=='') 
+    && (email!=='')
+    && (user!=='')
+    && (password!=='')
+    && (!Boolean(errorName)) 
+    && (!Boolean(errorUser)) 
+    && (!Boolean(errorEmail))
+    && (!Boolean(errorPassword))) {location.replace("/")};
+  };
+
   return (
     <MDBContainer fluid>
 
@@ -23,14 +91,15 @@ function SignUpComponent() {
               <h2 className="fw-bold mb-2 text-uppercase">User Register</h2>
               <p className="text-dark-50 mb-5">Please crate your account!</p>
 
-              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-dark' label='Name' id='formControlLg' type='txt' size="lg"/>
-              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-dark' label='Email address' id='formControlLg' type='email' size="lg"/>
-              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-dark' label='User Name' id='formControlLg' type='txt' size="lg"/>
-              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-dark' label='Password' id='formControlLg' type='password' size="lg"/>
-
-              <MDBBtn outline className='mx-2 px-5' color='success' size='lg' href="/">
-                Save
-              </MDBBtn>
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-dark' label='Name' id='name' type='txt' size="lg" value={name} onChange={changeHandlerName}/>
+              {Boolean(errorName) && <p>{errorName}</p>}
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-dark' label='Email address' id='email' type='email' size="lg" value={email} onChange={changeHandlerEmail} />
+              {Boolean(errorEmail) && <p>{errorEmail}</p>}
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-dark' label='User Name' id='user' type='txt' size="lg" value={user} onChange={changeHandlerUser}/>
+              {Boolean(errorUser) && <p>{errorUser}</p>}
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-dark' label='Password' id='password' type='password' size="lg" value={password} onChange={changeHandlerPassword}/>
+              {Boolean(errorPassword) && <p>{errorPassword}</p>}
+              <button type="button" class="btn btn-outline-success mx-2 px-5" onClick={submitHandler}> Save </button>
 
             </MDBCardBody>
           </MDBCard>
