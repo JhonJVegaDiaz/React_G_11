c6001ea00255f02a8f3ad2df2f6b2c7f5a646db4
JhonJVegaDiaz <jhonjairovegadiaz@gmail.com>
2022-09-22 12:04:06 -0500

    inclusion de Login y signup

--- InstaYa/src/App.jsx
+++ InstaYa/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginComponent from './components/login/login';
+import SignUpComponent from './components/signUp/signup';
 
 
 function App() {
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginComponent />} />
+          <Route path="/Login" element={<LoginComponent />} />
+          <Route path="/SignUp" element={<SignUpComponent />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ InstaYa/src/components/signUp/signup.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+}
+from 'mdb-react-ui-kit';
+
+function SignUpComponent() {
+  return (
+    <MDBContainer fluid>
+
+      <MDBRow className='d-flex justify-content-center align-items-center h-100'>
+        <MDBCol col='12'>
+
+          <MDBCard className='bg-dark text-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '400px'}}>
+            <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
+
+              <h2 className="fw-bold mb-2 text-uppercase">User Register</h2>
+              <p className="text-white-50 mb-5">Please crate your account!</p>
+
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Name' id='formControlLg' type='txt' size="lg"/>
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Email address' id='formControlLg' type='email' size="lg"/>
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='User Name' id='formControlLg' type='txt' size="lg"/>
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Password' id='formControlLg' type='password' size="lg"/>
+
+              <MDBBtn outline className='mx-2 px-5' color='success' size='lg' href="/">
+                Save
+              </MDBBtn>
+
+            </MDBCardBody>
+          </MDBCard>
+
+        </MDBCol>
+      </MDBRow>
+
+    </MDBContainer>
+  );
+}
+
+export default SignUpComponent;
